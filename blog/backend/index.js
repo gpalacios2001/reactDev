88c6fdfec8ee5a231c51be6730f4b5bc1af0a0d4
gpalacios2001 <gpalacios8258@gmail.com>
2024-06-06 17:03:57 -0700

    proper structure and connects to db

--- blog/backend/index.js
+++ blog/backend/index.js
@@ -1,20 +1,7 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app') // The Express app
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-
-
-const mongoUrl = 'mongodb+srv://gabrielpalacios:lola0827@cluster0.2iicupf.mongodb.net/Blogs?retryWrites=true&w=majority&appName=Cluster0'
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
